af70d592a9a639a63df3365019265f33b8a7bc5f
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-04-30 13:55:54 +0100

    done with first test

--- problem-solving/1-easy/simple-brackets.js
+++ problem-solving/1-easy/simple-brackets.js
@@ -1,13 +1,33 @@
 // Given a string s containing just the characters '(', ')', determine if the input string is valid.
 // 		- An input string is valid if: Open brackets must always be closed. Every close bracket has a corresponding open bracket.
-// 		- Example 1: Input: s = "()" Output: true 
-// 		- Example 2: Input: s = ")(" Output: false 
+// 		- Example 1: Input: s = "()" Output: true
+// 		- Example 2: Input: s = ")(" Output: false
 // 		- Example 3: Input: s = "(())" Output: true
 // 		- Example 4: Input: s = "()(()" Output: false
 
 // Test with `npm test -- simple-brackets.test.js`
 
 function isBalanced(str) {
-}
+  if (str === "") {
+    return true;
+  }
 
-module.exports = isBalanced
+  let counter = 0;
+
+  if (str != "") {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "(") {
+        counter ++;
+      } else if (str[i] === ")") {
+        counter --;
+
+        if (counter < 0) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return counter === 0;
+}
+module.exports = isBalanced;
